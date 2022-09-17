c3fe68bf56a070590b3224fb328b0f62ecc42410
Mehradi <mehradi.info@gmail.com>
2022-09-17 17:38:44 +0300

    Defining our graphql schema

--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,49 @@ import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
   type Query {
-    hello: String!
+    me: User
+    posts: [Post!]!
+    profile(userID: ID!): Profile
+  }
+  type Mutation {
+    postCreate(post: PostInput!): PostPayload!
+  }
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    published: Boolean!
+    createAt: String!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    name: String!
+    posts: [Post!]!
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    isMyProfile: Boolean!
+    user: User!
+  }
+
+  type UserError {
+    message: String!
+  }
+  type PostPayload {
+    userError: [userError!]!
+    post: Post
+  }
+  type AuthPayload {
+    userError: [UserError!]!
+    token: String!
+  }
+  input PostInput {
+    title: String!
+    content: String!
   }
 `;
