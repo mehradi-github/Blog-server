32087a0fde03780c404076a5ae81c56b5524230a
Mehradi <mehradi.info@gmail.com>
2022-09-18 13:38:32 +0300

    Adding query for me

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,14 @@
 import { Context } from '..';
 
 export const Query = {
+  me: (_: any, __: any, { userInfo, prisma }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     return await prisma.post.findMany({
       orderBy: [{ createAt: 'desc' }],
